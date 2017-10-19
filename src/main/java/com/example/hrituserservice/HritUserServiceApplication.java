5f1feae9096e304607664fbbb7ad43ce0499dcfb
Mikhail Nikitin <Mikhail_Nikitin@epam.com>
2017-10-19 12:17:13 +0300

    repository init

---
+++ src/main/java/com/example/hrituserservice/HritUserServiceApplication.java
@@ -0,0 +1,12 @@
+package com.example.hrituserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HritUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HritUserServiceApplication.class, args);
+	}
+}
